d442c07c638f1d7961b61b4ca899469c851279ed
FallFall29 <fallshoppstoree@gmail.com>
2026-03-13 20:56:44 +0700

    Update [key].js

--- api/callbackqris/[key].js
+++ api/callbackqris/[key].js
@@ -1,62 +1,37 @@
-import { savePayment, isRRNProcessed, markRRNProcessed } from "../../lib/payment.js"
+const { savePayment, isRRNProcessed, markRRNProcessed } = require("../../lib/payment.js")
 
-const SECRET   = process.env.CALLBACK_SECRET   || "fallshop"
-const USERNAME = process.env.QRIS_USERNAME     || "fallshop"
+const SECRET   = process.env.CALLBACK_SECRET || "fallshop"
+const USERNAME = process.env.QRIS_USERNAME   || "fallshop"
 
-export default async function handler(req, res) {
-
+module.exports = async (req, res) => {
   if (req.method !== "POST") {
-    return res.status(405).json({
-      responseCode: "405",
-      responseMessage: "Method not allowed"
-    })
+    return res.status(405).json({ responseCode: "405", responseMessage: "Method not allowed" })
   }
 
-  // Validasi secret key
   const { key } = req.query
   if (key !== SECRET) {
-    return res.status(401).json({
-      responseCode: "4015200",
-      responseMessage: "Unauthorized"
-    })
+    return res.status(401).json({ responseCode: "4015200", responseMessage: "Unauthorized" })
   }
 
   const data = req.body
 
-  // Validasi username
-  if (data.us_username !== USERNAME) {
-    return res.status(200).json({
-      responseCode: "4015200",
-      responseMessage: "Username invalid"
-    })
+  if (!data || data.us_username !== USERNAME) {
+    return res.status(200).json({ responseCode: "4015200", responseMessage: "Username invalid" })
   }
 
-  // Validasi RRN ada
   if (!data.rrn) {
-    return res.status(200).json({
-      responseCode: "4005200",
-      responseMessage: "RRN missing"
-    })
+    return res.status(200).json({ responseCode: "4005200", responseMessage: "RRN missing" })
   }
 
-  // Cek duplikat RRN
   const alreadyProcessed = await isRRNProcessed(data.rrn)
   if (alreadyProcessed) {
-    return res.status(200).json({
-      responseCode: "2005201",
-      responseMessage: "RRN already processed"
-    })
+    return res.status(200).json({ responseCode: "2005201", responseMessage: "RRN already processed" })
   }
 
   console.log("CALLBACK MASUK:", data)
 
-  // Simpan transaksi ke Redis & tandai RRN sudah diproses
   await savePayment(data.rrn, data)
   await markRRNProcessed(data.rrn)
 
-  return res.status(200).json({
-    responseCode: "2005200",
-    responseMessage: "Request has been processed successfully"
-  })
-
+  return res.status(200).json({ responseCode: "2005200", responseMessage: "Request has been processed successfully" })
 }
